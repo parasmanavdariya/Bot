b5144b1a4a0616346b52f5e803d2cc75a50860c3
parasmanavdariya <viruspatel0@gmail.com>
2019-12-07 20:50:50 +0530

    Update Bot.py
    project start

--- Bot.py
+++ Bot.py
@@ -6,7 +6,7 @@ import webbrowser
 import os
 import smtplib
 
-engine = pyttsx3.init('dummy')
+engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
